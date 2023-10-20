b221a2bf91c188fd7a9a5d8ae648ffff471bb86a
Natalie Esseen <natalieesseen@gmail.com>
2023-10-20 12:12:23 +0200

    add Header-component

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './header.module.scss'
+import Image from 'next/image'
+import heroImg from '../../../public/header.jpeg'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        <Image className={styles.bgImage} src={heroImg} />
+        <div className={styles.content}>
+            <h1>Genshin Dishes</h1>
+            <p>
+                Explore Genshin Impact's culinary delights! Dive into the wide range of delicious food items,
+                each with its own unique flavor and effects. From spicy skewers to sweet treats, our virtual kitchen has it all.
+                Find the perfect dish to enhance your gameplay and enjoy the journey!
+            </p>
+        </div>
+    </header>
+  )
+}
+
+export default Header
